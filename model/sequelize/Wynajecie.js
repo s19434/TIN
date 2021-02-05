244f4950d45feb163150d02bbfe8197090a3a07b
Oleksandr Rudenko <s19434@pjwstk.edu.pl>
2021-02-05 06:45:32 +0100

    Done errors in dates

--- model/sequelize/Wynajecie.js
+++ model/sequelize/Wynajecie.js
@@ -44,7 +44,23 @@ const Wynajecie = sequelize.define('Wynajecie', {
             },
             isDate: {
                 msg: "Pole powinno być datą"
-            }
+            },
+            inFuture: function (value) {
+                const valueDate = new Date(value);
+                const compareToDate = new Date();
+                if (valueDate.getTime() < compareToDate.getTime()) {
+                    throw new Error('Data wynajecia nie może być w przeszłości!');
+                }
+            },
+            inPast: function (value) {
+                const rentalDate = new Date(value);
+                const endDate = new Date(this.Data_Oddania);
+                if (rentalDate.getTime() > endDate.getTime()) {
+                    throw new Error('Data wynajęcia nie może być po dacie oddania!');
+                } else if (rentalDate.getTime() == endDate.getTime()) {
+                    throw new Error('Data wynajęcia nie może być w ten samy dzień co i data oddania!');
+                }
+            },
         }
     },
     Data_Oddania: {
@@ -56,7 +72,23 @@ const Wynajecie = sequelize.define('Wynajecie', {
             },
             isDate: {
                 msg: "Pole powinno być datą"
-            }
+            },
+            inFuture: function (value) {
+                const valueDate = new Date(value);
+                const compareToDate = new Date();
+                if (valueDate.getTime() < compareToDate.getTime()) {
+                    throw new Error('Data oddania nie może być w przeszłości!');
+                }
+            },
+            inPast: function (value) {
+                const rentalDate = new Date(this.Data_Wynajecia);
+                const endDate = new Date(value);
+                if (rentalDate > endDate.getTime()) {
+                    throw new Error('Data wynajęcia nie może być po dacie oddania!');
+                } else if (rentalDate.getTime() == endDate.getTime()) {
+                    throw new Error('Data wynajęcia nie może być w ten samy dzień co i data oddania!');
+                }
+            },
         }
     },
 
